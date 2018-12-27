49a112abff8b9d3cd70323e84314a3e2af10211e
vsanchezm <vsanchezm@codicesoftware.com>
2018-12-27 11:45:10 +0000

    Move CreateContentErrorPanel method from foo.cs to bar.cs

--- bar.cs
+++ bar.cs
@@ -76,6 +76,37 @@ namespace GitMaster.LoginWindow
             Login.Run(mRestApi, data, mProgressControls, mLoginSuccessNotifier);
         }
 
+        Panel CreateContentErrorPanel(string message)
+        {
+            StackPanel result = new StackPanel();
+
+            TextBlock titleTextBlock = WebControlBuilder.CreateTitle(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.WaitingLicensePanelErrorTitle));
+            titleTextBlock.Margin = new Thickness(0, 40, 0, 15);
+
+            WebErrorPanel errorPanel = new WebErrorPanel();
+            errorPanel.ShowError(message);
+
+            mTeamInvitationCodeTextBox = WebControlBuilder.CreateTextBox(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.WaitingLicensePanelTeamInvitationCodeWatermark));
+
+            mGetLicenseButton = WebControlBuilder.CreateMainActionButton(
+                GitMasterLocalization.GetString(
+                    GitMasterLocalization.Name.GetLicenseButtonUppercase));
+            mGetLicenseButton.Click += GetLicenseButton_Click;
+
+            WebEntriesPacker.AddRelatedComponents(
+                result,
+                titleTextBlock,
+                errorPanel,
+                mTeamInvitationCodeTextBox,
+                mGetLicenseButton);
+
+            return result;
+        }
+
         void ShowErrors(Login.ValidationResult result)
         {
             if (!string.IsNullOrEmpty(result.EncryptedPasswordError))
